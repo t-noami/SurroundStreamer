f3737f40f264bb54584854b9ba907ca682dcd727
t-noami <tia.rungray@gmail.com>
2026-05-07 20:26:58 +0900

    Remove legacy and web player from repository

--- legacy/SurroundStreamer-old-version/eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import eslintConfig from '@electron-toolkit/eslint-config'
-import eslintConfigPrettier from '@electron-toolkit/eslint-config-prettier'
-
-export default [
-  { ignores: ['**/node_modules', '**/dist', '**/out'] },
-  eslintConfig,
-  eslintConfigPrettier
-]
